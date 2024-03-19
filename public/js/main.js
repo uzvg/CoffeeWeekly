3cefd05bc0526bdf7f014f75a382fff4549b29da
uzvg <1497911983@qq.com>
2024-03-19 13:01:53 +0800

    regular update

--- public/js/main.js
+++ public/js/main.js
@@ -1483,14 +1483,3 @@ NexT.plugins.search.localsearch = function() {
     }
   });
 }
-
-;
-/* Google translate plugin */
-NexT.plugins.others.translate = function() {
-  const element = '#gtranslate';
-  if (!NexT.utils.checkDOMExist(element)) return;
-  NexT.utils.lazyLoadComponent(element, function() { 
-    window.translateelement_styles='/css/google-translate.min.css'; 
-    NexT.utils.getScript('/js/third-party/google-translate.min.js');
-  });
-}
